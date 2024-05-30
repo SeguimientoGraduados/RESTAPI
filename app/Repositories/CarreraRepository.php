5b1ebc17153bb5e91e9d92a48eb8b6d290241299
Dylan Hughes <dylanhughes@codimat.com.ar>
2024-05-30 12:58:48 -0300

    DTO para carreras de formulario

--- app/Repositories/CarreraRepository.php
+++ app/Repositories/CarreraRepository.php
@@ -3,11 +3,25 @@
 namespace App\Repositories;
 
 use App\Repositories\Interfaces\ICarreraRepository;
+use App\DTO\CarreraDeGraduadoParaFormularioDTO;
 use App\Models\Carrera;
 
-class CarreraRepository implements ICarreraRepository 
+class CarreraRepository implements ICarreraRepository
 {
-    public function obtenerTodasLasCarreras(){
-        return Carrera::all();
+    public function obtenerTodasLasCarreras()
+    {
+
+        $carreras = Carrera::select('id', 'nombre')->get();
+
+        $carrerasDTOs = $carreras->map(function ($carrera) {
+            return new CarreraDeGraduadoParaFormularioDTO(
+                $carrera->id,
+                $carrera->nombre
+            );
+        });
+
+        $result = $carrerasDTOs->toArray();
+
+        return $result;
     }
 }
